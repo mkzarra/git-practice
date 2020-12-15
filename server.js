1a589e9914d52b1568c2062dbd5d48c79f8d8c6d
famousPete <pete.macaluso@generalassemb.ly>
2020-12-15 16:47:35 -0500

    add more new routes

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
 	res.send('hello from root!!!')
 })
 
+app.get('/things', (req, res) => {
+	res.send('have some things!!!')
+})
+
 const PORT = process.env.PORT || 4147
 app.listen(PORT, () => {
 	console.log('listening on port: ', PORT);
